e827d963408cca95d86e6309038c5261b6fc337e
Alexander Schier <allo@laxu.de>
2020-04-19 00:40:45 +0200

    Added color filters

--- filters.py
+++ filters.py
@@ -14,9 +14,22 @@ def blur(frame, intensity_x=5, intensity_y=-1):
 def noop(frame, *args, **kwargs):
     return frame
 
+def single_color(frame, r=255.0, g=255.0, b=255.0):
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
+    return color_filter(frame, r, g, b)
+
+def color_filter(frame, r=255.0, g=255.0, b=255.0):
+    frame[:,:,0] = frame[:,:,1] * r / 255.0
+    frame[:,:,1] = frame[:,:,1] * g / 255.0
+    frame[:,:,2] = frame[:,:,2] * b / 255.0
+    return frame
+
 FILTERS = {
     "blur": blur,
-    "grayscale": grayscale
+    "grayscale": grayscale,
+    "single_color": single_color,
+    "color_filter": color_filter
 }
 
 def get_filter(name):
