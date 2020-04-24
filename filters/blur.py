3e5dab949d4c3ffac4f926462582d78b1f07edc5
Alexander Schier <allo@laxu.de>
2020-04-24 17:18:18 +0200

    Change check for intensity_y == -1 to intensity_y < 0 in the blur filter

--- filters/blur.py
+++ filters/blur.py
@@ -5,7 +5,7 @@ def blur(intensity_x=5, intensity_y=-1, *args, **kwargs):
     frame = kwargs['frame']
     if intensity_x <= 0 and intensity_y <= 0:
         return frame
-    if intensity_y == -1:
+    if intensity_y < 0:
         intensity_y = intensity_x
     return cv2.blur(frame, (intensity_x, intensity_y))
 
