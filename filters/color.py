31abf21b06b313cd7e0966001e4980ea919d4b69
Alexander Schier <allo@laxu.de>
2020-04-28 17:25:30 +0200

    Fixed KeyError in single_color filter (Fixes #18)
    - Fixed KeyError in single_color filter
    - Fixed a wrong index in color_filter

--- filters/color.py
+++ filters/color.py
@@ -5,11 +5,12 @@ def single_color(r=255.0, g=255.0, b=255.0, *args, **kwargs):
     frame = kwargs['frame']
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
-    return color_filter(frame, r, g, b)
+    kwargs['frame'] = frame
+    return color_filter(r=r, g=g, b=b, *args, **kwargs)
 
 def color_filter(r=255.0, g=255.0, b=255.0, *args, **kwargs):
     frame = kwargs['frame']
-    frame[:,:,0] = frame[:,:,1] * r / 255.0
+    frame[:,:,0] = frame[:,:,0] * r / 255.0
     frame[:,:,1] = frame[:,:,1] * g / 255.0
     frame[:,:,2] = frame[:,:,2] * b / 255.0
     return frame
