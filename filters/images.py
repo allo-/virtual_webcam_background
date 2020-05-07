378374f392076a1c2815a06a4de497de1ea9d7af
Alexander Schier <allo@laxu.de>
2020-05-07 21:49:28 +0200

    Added ImageSequence filter

--- filters/images.py
+++ filters/images.py
@@ -2,6 +2,40 @@ import cv2
 import filters
 import os
 import stat
+import glob
+import time
+
+
+def reload_images(images_path, mtime, width, height, interpolation_method):
+    # Do nothing, if the image is unchanged
+    images_stat = os.stat(images_path)
+    if images_stat.st_mtime == mtime:
+        return None, mtime
+    mtime_new = images_stat.st_mtime
+
+    if stat.S_ISDIR(images_stat.st_mode):
+        filenames = glob.glob(images_path + "/*.*")
+    else:
+        filenames = [images_path]
+
+    images = []
+    for filename in filenames:
+        image_raw = cv2.imread(filename, cv2.IMREAD_UNCHANGED)
+
+        _interpolation_method = cv2.INTER_LINEAR
+        if interpolation_method == "NEAREST":
+            _interpolation_method = cv2.INTER_NEAREST
+
+        image = cv2.resize(image_raw, (width, height),
+                           interpolation=_interpolation_method)
+        if len(image.shape) == 2:  # grayscale image
+            image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+
+        # BGR to RGB
+        image[:,:,0], image[:,:,2] = image[:,:,2], image[:,:,0].copy()
+        images.append(image)
+
+    return images, mtime_new
 
 
 class Image:
@@ -19,30 +53,51 @@ class Image:
         self.reload_image()
 
     def reload_image(self):
-        # Do nothing, if the image is unchanged
-        image_stat = os.stat(self.image_path)
-        if image_stat.st_mtime == self.mtime:
-            return
-        self.mtime = image_stat.st_mtime
+        images, new_mtime = reload_images(self.image_path, self.mtime,
+                self.width, self.height, self.interpolation_method)
 
-        image_raw = cv2.imread(self.image_path, cv2.IMREAD_UNCHANGED)
-
-        interpolation_method = cv2.INTER_LINEAR
-        if self.interpolation_method == "NEAREST":
-            interpolation_method = cv2.INTER_NEAREST
-
-        image = cv2.resize(image_raw, (self.width, self.height),
-                           interpolation=interpolation_method)
-        if len(image.shape) == 2:  # grayscale image
-            image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
-
-        # BGR to RGB
-        image[:,:,0], image[:,:,2] = image[:,:,2], image[:,:,0].copy()
-        self.image = image
+        if images:
+            self.image = images[0]
+            self.mtime = new_mtime
 
     def apply(self, *args, **kwargs):
         self.reload_image()
         return self.image
 
 
+class ImageSequence:
+    def __init__(self, images_path, fps=10, interpolation_method="NEAREST",
+                 *args, **kwargs):
+        config = kwargs['config']
+        self.width = config.get("width")
+        self.height = config.get("height")
+        assert(self.width is not None and self.height is not None)
+
+        self.images_path = images_path
+        self.fps = fps
+        self.interpolation_method = interpolation_method
+        self.mtime = 0
+
+        self.reload_images()
+
+    def reload_images(self):
+        images, new_mtime = reload_images(self.images_path, self.mtime,
+                self.width, self.height, self.interpolation_method)
+
+        if images:
+            self.images = images
+            self.mtime = new_mtime
+            self.idx = 0
+            self.last_frame_time = time.time()
+
+    def apply(self, *args, **kwargs):
+        self.reload_images()
+        frame = self.images[self.idx]
+        if time.time() - self.last_frame_time > 1.0 / self.fps:
+            self.idx = (self.idx + 1) % len(self.images)
+            self.last_frame_time = time.time()
+        return frame
+
+
 filters.register_filter("image", Image)
+filters.register_filter("image_sequence", ImageSequence)
